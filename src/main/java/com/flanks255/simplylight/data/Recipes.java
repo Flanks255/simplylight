9aa6fd073cfd2448619c15ef4cf0be9bc743d3e3
Flanks255 <32142731+Flanks255@users.noreply.github.com>
2020-11-10 20:20:56 -0600

    More datagens!

--- src/main/java/com/flanks255/simplylight/data/Recipes.java
+++ src/main/java/com/flanks255/simplylight/data/Recipes.java
@@ -1,10 +1,12 @@
 package com.flanks255.simplylight.data;
 
+import com.flanks255.simplylight.SimplyLight;
 import com.google.gson.JsonObject;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.DirectoryCache;
-import net.minecraft.data.IFinishedRecipe;
-import net.minecraft.data.RecipeProvider;
+import net.minecraft.advancements.criterion.ImpossibleTrigger;
+import net.minecraft.data.*;
+import net.minecraft.item.Items;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.Tags;
 
 import java.nio.file.Path;
 import java.util.function.Consumer;
@@ -16,6 +18,71 @@ public class Recipes extends RecipeProvider {
 
     @Override
     protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
+        // Illuminant Block (Off)
+        ShapedRecipeBuilder.shapedRecipe(SimplyLight.ITEM_ILLUMINANTBLOCK.get(), 4)
+                .patternLine("aba")
+                .patternLine("bcb")
+                .patternLine("aba")
+                .key('a', Tags.Items.STONE)
+                .key('b', Items.GLOWSTONE)
+                .key('c', Tags.Items.DUSTS_REDSTONE)
+                .addCriterion("", hasItem(Items.AIR))
+                .build(consumer, new ResourceLocation(SimplyLight.MODID, "illuminant_block"));
+
+        // Illuminant Block (On)
+        ShapedRecipeBuilder.shapedRecipe(SimplyLight.ITEM_ILLUMINANTBLOCK_ON.get(), 4)
+                .patternLine("aba")
+                .patternLine("bcb")
+                .patternLine("aba")
+                .key('a', Tags.Items.STONE)
+                .key('b', Items.GLOWSTONE)
+                .key('c', Items.REDSTONE_TORCH)
+                .addCriterion("", hasItem(Items.AIR))
+                .build(consumer, new ResourceLocation(SimplyLight.MODID, "illuminant_block_on"));
+
+        // Illuminant Block (On) from (Off)
+        ShapelessRecipeBuilder.shapelessRecipe(SimplyLight.ITEM_ILLUMINANTBLOCK_ON.get(), 1)
+                .addIngredient(SimplyLight.ITEM_ILLUMINANTBLOCK.get())
+                .addCriterion("", hasItem(Items.AIR))
+                .build(consumer, new ResourceLocation(SimplyLight.MODID, "illuminant_block_on_from_off"));
+
+        // Illuminant Block (Off) from (On)
+        ShapelessRecipeBuilder.shapelessRecipe(SimplyLight.ITEM_ILLUMINANTBLOCK.get(), 1)
+                .addIngredient(SimplyLight.ITEM_ILLUMINANTBLOCK_ON.get())
+                .addCriterion("", hasItem(Items.AIR))
+                .build(consumer, new ResourceLocation(SimplyLight.MODID, "illuminant_block_off_from_on"));
+
+        // Bulbs
+        ShapedRecipeBuilder.shapedRecipe(SimplyLight.ITEM_LIGHTBULB.get(), 8)
+                .patternLine(" b ")
+                .patternLine("aaa")
+                .key('a', Tags.Items.STONE)
+                .key('b', Items.GLOWSTONE)
+                .addCriterion("", hasItem(Items.AIR))
+                .build(consumer, new ResourceLocation(SimplyLight.MODID, "bulb"));
+
+        // Edge light
+        ShapedRecipeBuilder.shapedRecipe(SimplyLight.ITEM_EDGELAMP.get(), 6)
+                .patternLine("b b")
+                .patternLine("aaa")
+                .patternLine("b b")
+                .key('a', Items.GLOWSTONE)
+                .key('b', Tags.Items.STONE)
+                .addCriterion("", hasItem(Items.AIR))
+                .build(consumer, new ResourceLocation( SimplyLight.MODID, "edge_light"));
+
+        // Top Edge light from bottom
+        ShapelessRecipeBuilder.shapelessRecipe(SimplyLight.ITEM_EDGELAMP_TOP.get(), 1)
+                .addIngredient(SimplyLight.ITEM_EDGELAMP.get())
+                .addCriterion("", hasItem(Items.AIR))
+                .build(consumer, new ResourceLocation(SimplyLight.MODID, "edge_light_top"));
+
+        // Bottom Edge light from top
+        ShapelessRecipeBuilder.shapelessRecipe(SimplyLight.ITEM_EDGELAMP.get(), 1)
+                .addIngredient(SimplyLight.ITEM_EDGELAMP_TOP.get())
+                .addCriterion("", hasItem(Items.AIR))
+                .build(consumer, new ResourceLocation(SimplyLight.MODID, "edge_light_bottom_from_top"));
+
 
     }
 
