c5491c793c0627ef095d020d583a5d9eae90d073
Flanks255 <32142731+Flanks255@users.noreply.github.com>
2020-11-21 08:47:53 -0600

    Fixed edge lights inventory perspective.

--- src/main/java/com/flanks255/simplylight/data/ItemModels.java
+++ src/main/java/com/flanks255/simplylight/data/ItemModels.java
@@ -24,27 +24,52 @@ public class ItemModels extends ItemModelProvider {
     private void registerBlockItem(Block block) {
         String path = block.getRegistryName().getPath();
         // Edge lights, rotate them so you can see them.
-        if (block == SimplyLight.EDGELAMP.get() || block == SimplyLight.EDGELAMP_TOP.get()){
-            getBuilder(path).parent(new ModelFile.UncheckedModelFile(modLoc("block/edge_light")))
-                    .transforms().transform(ModelBuilder.Perspective.GUI)
-                    .rotation(30, -135, 180)
-                    .scale(0.625f, 0.625f, 0.625f)
-                    .end()
-                    .transform(ModelBuilder.Perspective.FIRSTPERSON_RIGHT)
-                    .rotation(-45,0,90)
-                    .translation(-7.5f, 6.5f, 4.5f)
-                    .scale(1f,1f,1f)
-                    .end()
-                    .transform(ModelBuilder.Perspective.THIRDPERSON_RIGHT)
-                    .rotation(-45,0,90)
-                    .translation(-7.5f, 6.5f, 4.5f)
-                    .scale(1f,1f,1f)
-                    .end();
+        if (block == SimplyLight.EDGELAMP.get()){
+            registerEdgeBlockBottom(path);
+        } else if (block == SimplyLight.EDGELAMP_TOP.get()) {
+            registerEdgeBlockTop(path);
         }
         // All the rest are fine
         else {
             getBuilder(path).parent(new ModelFile.UncheckedModelFile(modLoc("block/" + path)));
         }
+    }
+
+    private void registerEdgeBlockTop(String path) {
+        getBuilder(path).parent(new ModelFile.UncheckedModelFile(modLoc("block/edge_light")))
+                .transforms().transform(ModelBuilder.Perspective.GUI)
+                .rotation(30, -135, 180)
+                .scale(0.625f, 0.625f, 0.625f)
+                .end()
+                .transform(ModelBuilder.Perspective.FIRSTPERSON_RIGHT)
+                .rotation(-45,0,90)
+                .translation(-7.5f, 6.5f, 4.5f)
+                .scale(1f,1f,1f)
+                .end()
+                .transform(ModelBuilder.Perspective.THIRDPERSON_RIGHT)
+                .rotation(-45,0,90)
+                .translation(-7.5f, 6.5f, 4.5f)
+                .scale(1f,1f,1f)
+                .end();
+    }
+
+    private void registerEdgeBlockBottom(String path) {
+        getBuilder(path).parent(new ModelFile.UncheckedModelFile(modLoc("block/edge_light")))
+                .transforms().transform(ModelBuilder.Perspective.GUI)
+                .rotation(30, -135, 180)
+                .translation(0,-6,0)
+                .scale(0.625f, 0.625f, 0.625f)
+                .end()
+                .transform(ModelBuilder.Perspective.FIRSTPERSON_RIGHT)
+                .rotation(-45,0,90)
+                .translation(-7.5f, 6.5f, 4.5f)
+                .scale(1f,1f,1f)
+                .end()
+                .transform(ModelBuilder.Perspective.THIRDPERSON_RIGHT)
+                .rotation(-45,0,90)
+                .translation(-7.5f, 6.5f, 4.5f)
+                .scale(1f,1f,1f)
+                .end();
     }
 }
 
