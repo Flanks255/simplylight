7487f253d9362bd53024e86d9adfcbd6c91aaeaa
Flanks255 <32142731+Flanks255@users.noreply.github.com>
2021-04-27 19:24:25 -0500

    Cleaning up BaseBlockItem tooltip code.

--- src/main/java/com/flanks255/simplylight/blocks/BaseBlockItem.java
+++ src/main/java/com/flanks255/simplylight/blocks/BaseBlockItem.java
@@ -8,13 +8,13 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.List;
 
@@ -25,22 +25,19 @@ public class BaseBlockItem extends BlockItem {
         this.block = blockIn;
     }
 
-    private Block block;
-
-    private boolean hasTranslation(String key) {
-        return !I18n.format(key).equals(key);
-    }
+    private final Block block;
 
     @OnlyIn(Dist.CLIENT)
     @Override
-    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+    public void addInformation(@Nonnull ItemStack stack, @Nullable World worldIn, @Nonnull List<ITextComponent> tooltip, @Nonnull ITooltipFlag flagIn) {
         super.addInformation(stack, worldIn, tooltip, flagIn);
-        if (Screen.hasShiftDown()) {
+        if (Screen.hasShiftDown() && I18n.hasKey(block.getTranslationKey() + ".info")) {
             tooltip.add(new TranslationTextComponent(block.getTranslationKey() + ".info"));
-            if (hasTranslation(block.getTranslationKey()+".info2"))
+            if (I18n.hasKey(block.getTranslationKey()+".info2")) {
                 tooltip.add(new TranslationTextComponent(block.getTranslationKey() + ".info2"));
-            if (hasTranslation(block.getTranslationKey()+".info3"))
-                tooltip.add(new TranslationTextComponent(block.getTranslationKey() + ".info3"));
+                if (I18n.hasKey(block.getTranslationKey() + ".info3"))
+                    tooltip.add(new TranslationTextComponent(block.getTranslationKey() + ".info3"));
+            }
         }
         else {
             tooltip.add(new TranslationTextComponent("simplylight.shift", new TranslationTextComponent("simplylight.key.shift").mergeStyle(TextFormatting.GOLD, TextFormatting.ITALIC)));
