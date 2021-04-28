98a40869a2339605146f9ed2707e4afc4cbfffdd
Flanks255 <32142731+Flanks255@users.noreply.github.com>
2021-04-27 19:24:55 -0500

    Implementing JEI Info Tab framework.

---
+++ src/main/java/com/flanks255/simplylight/JEIPlugin.java
@@ -0,0 +1,35 @@
+package com.flanks255.simplylight;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TextFormatting;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.annotation.Nonnull;
+
+@JeiPlugin
+public class JEIPlugin implements IModPlugin {
+    public static final ResourceLocation ID = new ResourceLocation(SimplyLight.MODID, "jei_plugin");
+    @Nonnull
+    @Override
+    public ResourceLocation getPluginUid() {
+        return ID;
+    }
+
+    @Override
+    public void registerRecipes(@Nonnull IRecipeRegistration registration) {
+        SimplyLight.ITEMS.getEntries().forEach((item) -> {
+            String key = item.get().getTranslationKey()+".jei.info";
+            if (I18n.hasKey(key)) {
+                String langEntry = TextFormatting.getTextWithoutFormattingCodes(I18n.format(key));
+                if (langEntry != null)
+                    registration.addIngredientInfo(new ItemStack(item.get()), VanillaTypes.ITEM, StringUtils.splitByWholeSeparator(langEntry, "//n"));
+            }
+        });
+    }
+}
